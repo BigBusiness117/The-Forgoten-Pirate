0fe564e0ce26b58ff2dbfbf84821fa67926cea72
BigBusiness117 <67163482+BigBusiness117@users.noreply.github.com>
2021-01-30 16:08:55 -0800

    the forgoten pirate
    added a third person movement scrpit and have a moveable character

---
+++ Assets/ThirdPersonMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 5f;
+    public Transform cam;
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+    public float gravity = -9.81f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    Vector3 Velocity;
+
+    bool isGrounded;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+       if (isGrounded && Velocity.y < 0)
+        {
+            Velocity.y = -2f;
+        }
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+            Velocity.y += gravity * Time.deltaTime;
+
+            controller.Move(Velocity * Time.deltaTime);
+        if(direction.magnitude >= 0.1f)
+        {
+            float targetAngel = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngel, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, targetAngel, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngel, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed  * Time.deltaTime);
+
+        }
+        
+    }
+}
